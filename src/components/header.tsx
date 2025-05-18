8d63a877984840f5950592ea93f84b6f4ef4e004
raoneinet <raoneinet@hotmail.com>
2025-05-18 21:54:54 +0100

    fix display movies and responsiveness

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,15 +4,15 @@ export const Header = ()=>{
     return (
         <header className="bg-black py-4 fixed w-full">
             <div className="container mx-auto h-full flex justify-around items-center">
-                <div className="flex items-center gap-2 h-full text-gray-200">
+                <div className="flex flex-col md:flex-row items-center gap-2 h-full text-gray-200">
                     <img src="/assets/icons/movie-icon.png" className="w-10"/>
                     <h1>Moviepedia</h1>
                 </div>
                 <div className="flex items-center">
                     <input 
                         type="text" 
-                        placeholder="Search a music title..."
-                        className="border border-gray-300 rounded-full py-2 px-3 w-95 text-white"/>
+                        placeholder="Search..."
+                        className="border border-gray-300 rounded-full py-2 px-3 w-32 md:w-78 text-white"/>
                     <button className="-ml-10 cursor-pointer"><img src="/assets/icons/search.svg" className="w-7 h-7"/></button>
                 </div>
             </div>
--- src/components/movieSection.tsx
+++ src/components/movieSection.tsx
@@ -6,16 +6,16 @@ export const MovieSection = ()=>{
     const movieCtx = useContext(apiContext)
 
     return (
-        <div className="mt-18">
+        <div className="my-18">
             <div className="container mx-auto pt-15">
-                <h1>Popular Movies</h1>
-                <div className="grid lg:grid-cols-7 gap-2">
+                <h1 className="font-bold text-lg text-gray-500 p-4">Popular Movies</h1>
+                <div className="grid lg:grid-cols-7 gap-2 justify-center">
                     {movieCtx?.popMovies?.map(item => (
-                        <div className="w-52 p-2 border border-gray-300 ">
+                        <div className="w-52 p-2 border border-gray-200 rounded-md">
                             <div>
                                 <img src={`https://media.themoviedb.org/t/p/w220_and_h330_face${item.poster_path}`}/>
                             </div>
-                            <h1>{item.title}</h1>
+                            <h1 className="font-bold pt-3">{item.title}</h1>
                         </div>
                     ))}
                 </div>
