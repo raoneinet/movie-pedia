657096d5ed38eed53152e5edb00aa54efab6867a
raoneinet <raoneinet@hotmail.com>
2025-05-27 23:43:37 +0100

    adding loading to movie screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,21 @@
 "use client"
 import {Header} from "@/components/header"
 import {PopularMoviesCtx} from "@/contexts/popularMoviesCtx"
-import {PopularMovies} from "@/components/popularMovies"
 import {TrendingProvider} from "@/contexts/trendingAllCtx"
-import {TrendingSection} from "@/components/trendingAll"
 import {SearchCtxProvider} from "@/contexts/searchContext"
+import {HomeMovies} from "@/components/homeMovies"
 
 const Page = ()=>{
-    return (
-        
+
+    return (    
         <div className="overflow-hidden w-full">
             <SearchCtxProvider>
                 <Header/>
                 <PopularMoviesCtx>
-                    <PopularMovies/>
-                </PopularMoviesCtx>
-                <TrendingProvider>    
-                    <TrendingSection/>
+                <TrendingProvider> 
+                    <HomeMovies/>
                 </TrendingProvider>
+                </PopularMoviesCtx>
             </SearchCtxProvider>
         </div>
     )
