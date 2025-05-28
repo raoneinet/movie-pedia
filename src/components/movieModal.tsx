8f6fad53dde894c3d06b2bdec9a7206e45a032eb
raoneinet <raoneinet@hotmail.com>
2025-05-28 01:50:54 +0100

    creating modal for selelected movie

--- src/components/movieModal.tsx
+++ src/components/movieModal.tsx
@@ -1,9 +1,36 @@
+import { MovieType } from "@/types/movieType"
 
+type Props = {
+    movie: MovieType[]
+    closeModal: () => void
+}
 
-export const MovieCard = ()=>{
+export const MovieCard = ({ movie, closeModal }: Props) => {
     return (
         <div>
-            
+            {movie.map(item =>
+                <div key={item.id} className="w-screen h-1/3 grid -mt-5 py-6 bg-[#202020]">
+                    <div 
+                        className="text-white text-xl font-bold mr-8 p-2 cursor-pointer rounded-full border-3 inline place-self-end" 
+                        onClick={closeModal}>X</div>
+                    <div key={item.id} className="w-3xl flex gap-4 m-auto text-white">
+                        <div className="w-64">
+                            <img className="w-64 rounded-md"
+                                src={`https://media.themoviedb.org/t/p/w220_and_h330_face${item.poster_path}`}
+                            />
+                            <h1 className="text-lg wrap font-bold">{item.title ? item.title : item.name}</h1>
+                        </div>
+                        <div className="flex-1 capitalize">
+                            <p className="text-2xl font-bold mb-4">{item.original_title}</p>
+                            <p>{item.adult === false && "Adulto"}</p>
+                            <p><span className="font-bold">Idioma original:</span> {item.original_language}</p>
+                            <p><span className="font-bold">Data de lançamento:</span> {item.release_date}</p>
+                            <p><span className="font-bold">Avaliação:</span> {item.vote_average}</p>
+                            <p><span className="font-bold">Sinopse:</span> <span className="text-sm">{item.overview}</span></p>
+                        </div>
+                    </div>
+                </div>
+            )}
         </div>
     )
 }
--- src/components/popularMovies.tsx
+++ src/components/popularMovies.tsx
@@ -1,15 +1,17 @@
-import { useContext, useRef } from "react"
+import { useContext, useRef, useState } from "react"
 import { apiContext } from "@/contexts/popularMoviesCtx"
+import { MovieCard } from "./movieModal"
 
 export const PopularMovies = () => {
 
     const movieCtx = useContext(apiContext)
-
     const movieContainerRef = useRef<HTMLDivElement>(null)
+    const [selectedMovie, setSelectedMovie] = useState<any>([])
+    const [clickedBtn, setClickedBtn] = useState<boolean>(true)
 
-    const handleLeft = ()=> {
-        if(movieCtx?.popMovies?.length && movieContainerRef.current){
-            if(movieContainerRef){
+    const handleLeft = () => {
+        if (movieCtx?.popMovies?.length && movieContainerRef.current) {
+            if (movieContainerRef) {
                 movieContainerRef.current.scrollBy({
                     left: -200,
                     behavior: "smooth"
@@ -29,33 +31,48 @@ export const PopularMovies = () => {
         }
     }
 
+    const sendClickedMovie = (id: number) => {
+        setClickedBtn(clickedBtn)
+        console.log(id)
+        const selectMovie = movieCtx?.popMovies?.filter(item => id === item.id)
+        setSelectedMovie(selectMovie)
+        console.log(selectMovie)
+    }
+
+    const closeMovieModal = ()=>{
+        setSelectedMovie([])
+    }
+
     return (
         <div className="mt-5">
-            <div className="container mx-auto pt-15">
-                <h1 className="font-bold text-lg text-gray-500 p-4">Popular Movies</h1>
-                <div 
-                    className="flex flex-nowrap gap-2 w-full overflow-x-hidden mask-x-from-85%" 
-                    ref={movieContainerRef}
-                >
-                    {movieCtx?.popMovies?.map(item => (
-                        <div className="w-40 flex-none px-2 rounded-md" key={item.id}>
-                            <div>
-                                <img className="inline-block rounded-md" src={`https://media.themoviedb.org/t/p/w220_and_h330_face${item.poster_path}`} />
-                            </div>
-                            <h1 className="font-extrabold pt-3 text-sm">{item.title ? item.title : item.name}</h1>
-                            <p className="text-sm text-gray-500">{item.release_date ? item.release_date : item.first_air_date}</p>
+            {clickedBtn && <MovieCard movie={selectedMovie} closeModal={closeMovieModal}/>}
+                <>
+                    <div className="container mx-auto pt-15">
+                        <h1 className="font-bold text-lg text-gray-500 p-4">Popular Movies</h1>
+                        <div
+                            className="flex flex-nowrap gap-2 w-full overflow-x-hidden mask-x-from-85%"
+                            ref={movieContainerRef}
+                        >
+                            {movieCtx?.popMovies?.map(item => (
+                                <div className="w-40 flex-none px-2 rounded-md cursor-pointer" key={item.id} onClick={() => sendClickedMovie(item.id)}>
+                                    <div>
+                                        <img className="inline-block rounded-md" src={`https://media.themoviedb.org/t/p/w220_and_h330_face${item.poster_path}`} />
+                                    </div>
+                                    <h1 className="font-extrabold pt-3 text-sm">{item.title ? item.title : item.name}</h1>
+                                    <p className="text-sm text-gray-500">{item.release_date ? item.release_date : item.first_air_date}</p>
+                                </div>
+                            ))}
                         </div>
-                    ))}
-                </div>
-                <div className="w-full inline-flex justify-between py-3 relative -top-55">
-                    <button className="cursor-pointer w-10" onClick={handleLeft}>
-                        <img src="/assets/icons/arrow-left.png"/>
-                    </button>
-                    <button className="cursor-pointer w-10" onClick={handleRight}>
-                        <img src="/assets/icons/arrow-right.png"/>
-                    </button>
-                </div>
-            </div>
+                        <div className="w-full inline-flex justify-between py-3 relative -top-55">
+                            <button className="cursor-pointer w-10" onClick={handleLeft}>
+                                <img src="/assets/icons/arrow-left.png" />
+                            </button>
+                            <button className="cursor-pointer w-10" onClick={handleRight}>
+                                <img src="/assets/icons/arrow-right.png" />
+                            </button>
+                        </div>
+                    </div>
+                </>
         </div>
     )
 }
