c04c62b6cf896ea64717ddf26e47498902a3d6dd
raoneinet <raoneinet@hotmail.com>
2025-06-03 16:04:01 +0100

    mb- added in items search

--- src/components/searchResponse.tsx
+++ src/components/searchResponse.tsx
@@ -14,15 +14,15 @@ export const SearchResponse = () => {
     return (
         <div className="mt-5 lg:w-7xl mx-auto">
             <div className="flex justify-between items-end">
-                <div className="text-lg">Resultado da pesquisa</div>
+                <div className="text-lg text-gray-500 font-bold">Resultado da pesquisa</div>
                 <div
-                    className="text-xl font-bold mr-8 p-2 cursor-pointer rounded-full border-1 inline justify-self-end"
+                    className="text-xl font-bold mr-8 p-2 cursor-pointer justify-self-end"
                     onClick={closeSearch}>Fechar pesquisa
                 </div>
             </div>
             <div className="container mx-auto flex flex-wrap mt-6">
                 {srcRes?.searchSuccess === 200 && srcRes?.searcRes.map(item => (
-                    <div className="w-40 flex-none px-2 rounded-md cursor-pointer" key={item.id}>
+                    <div className="w-40 flex-none px-2 rounded-md cursor-pointer mb-4" key={item.id}>
                         <div>
                             <img className="inline-block rounded-md" src={`https://media.themoviedb.org/t/p/w220_and_h330_face${item.poster_path}`} />
                         </div>
